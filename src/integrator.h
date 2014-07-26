c2cd50e5b8da86621fb93517352e7d306af99f2e
Anand Pratap Singh <anandps@f22.me>
2014-07-26 18:37:22 -0400

    Added euler and jst timestepping schemes, cfl condition is fixed at the moment

---
+++ src/integrator.h
@@ -0,0 +1,50 @@
+#ifndef _INCL_INTEGRATOR
+#define _INCL_INTEGRATOR
+#include "residual.h"
+
+void euler(int ni, int nj, double Q[ni-1][nj-1][4],			\
+	   double neta_x[ni-1][nj], double neta_y[ni-1][nj],		\
+	   double nxi_x[ni][nj-1], double nxi_y[ni][nj-1],		\
+	   double vol[ni-1][nj-1], double ds_eta[ni-1][nj-1], double ds_xi[ni-1][nj-1], \
+	   struct state freestream, double* res_rho, double gamma = 1.4){
+  
+  int nim = ni - 1;
+  int njm = nj - 1;
+  double res[nim][njm][4];
+  double dt[nim][njm];
+  
+  residual(ni, nj, Q, neta_x, neta_y, nxi_x, nxi_y, res, vol, ds_eta, ds_xi, dt, freestream, gamma);
+  for(int i=0; i< 4; i++)
+    Q[:][:][i] = Q[:][:][i] + res[:][:][i]/vol[:][:]*((dt[:][:]))*.8;
+  
+
+  *res_rho = sqrt(__sec_reduce_add(pow(res[:][:][0],2)));
+
+  
+}
+
+void jst(int ni, int nj, double Q[ni-1][nj-1][4],			\
+	 double neta_x[ni-1][nj], double neta_y[ni-1][nj],		\
+	 double nxi_x[ni][nj-1], double nxi_y[ni][nj-1],		\
+	 double vol[ni-1][nj-1], double ds_eta[ni-1][nj-1], double ds_xi[ni-1][nj-1], \
+	 struct state freestream, double* res_rho, double gamma = 1.4){
+  
+  int nim = ni - 1;
+  int njm = nj - 1;
+  double res[nim][njm][4];
+  double dt[nim][njm];
+  int order = 4;
+  for(int n=0; n< order; n++){
+    residual(ni, nj, Q, neta_x, neta_y, nxi_x, nxi_y, res, vol, ds_eta, ds_xi, dt, freestream, gamma);
+    for(int i=0; i< 4; i++)
+      Q[:][:][i] = Q[:][:][i] + res[:][:][i]/vol[:][:]*(dt[:][:])/(order-n)*2.0;
+  }
+
+  *res_rho = sqrt(__sec_reduce_add(pow(res[:][:][0],2)));
+
+  
+}
+
+
+
+#endif
