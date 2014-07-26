e875705f895a847a52b6a33d393ed7b25345fe98
Anand Pratap Singh <anandps@f22.me>
2014-07-26 18:46:27 -0400

    Added calculations of face length to be used to calculate timestepping

--- src/metric.h
+++ src/metric.h
@@ -1,12 +1,13 @@
 #ifndef _INCL_METRIC
 #define _INCL_METRIC
 #include <stdio.h>
-
+#include <cmath>
 // TEST STATUS: CHECKED DEFCON 1
 
 void metric(int ni, int nj, double x[ni][nj], double y[ni][nj], double neta_x[ni-1][nj], double neta_y[ni-1][nj], \
 	    double nxi_x[ni][nj-1], double nxi_y[ni][nj-1], double vol[ni-1][nj-1], \
-	    double xc[ni-1][nj-1], double yc[ni-1][nj-1]){
+	    double xc[ni-1][nj-1], double yc[ni-1][nj-1],		\
+	    double ds_eta[ni-1][nj-1], double ds_xi[ni-1][nj-1]){
 
   // Calculate face normals, cell volume, and cell center
   // Number of cells = (ni-1) x (nj-1) and faces are numbered accordingly
@@ -36,8 +37,14 @@ void metric(int ni, int nj, double x[ni][nj], double y[ni][nj], double neta_x[ni
 
   vol[0:nim][0:njm] = 0.5*(reta_x[0:nim][0:njm]*rxi_y[0:nim][0:njm] - rxi_x[0:nim][0:njm]*reta_y[0:nim][0:njm] \
 			   + reta_x[0:nim][1:njm]*rxi_y[1:nim][0:njm] - rxi_x[1:nim][0:njm]*reta_y[0:nim][1:njm]);
+
+
   xc[0:nim][0:njm] = 0.25*(x[0:nim][0:njm] + x[1:nim][0:njm] + x[0:nim][1:njm] + x[1:nim][1:njm]);
   yc[0:nim][0:njm] = 0.25*(y[0:nim][0:njm] + y[1:nim][0:njm] + y[0:nim][1:njm] + y[1:nim][1:njm]);
+
+  ds_eta[0:nim][0:njm] = sqrt(pow(neta_x[0:nim][0:njm],2) + pow(neta_y[0:nim][0:njm],2));
+  ds_xi[0:nim][0:njm] = sqrt(pow(nxi_x[0:nim][0:njm],2) + pow(nxi_y[0:nim][0:njm],2));
+  
 }
 
 #endif
