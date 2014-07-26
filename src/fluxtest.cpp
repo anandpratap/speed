becc59d9aa1fa3d9074090b2e9a03f910e2cbdec
Anand Pratap Singh <anandps@vector.engin.umich.edu>
2014-07-25 23:10:57 -0400

    Added roeflux function which support simd

---
+++ src/fluxtest.cpp
@@ -0,0 +1,28 @@
+#include "flux.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(void){
+  double gamma = 1.4;
+  double rho, u, v, p, f[4];
+  f[:] = 10.0;
+  rho = 1.0;
+  u = 1.0;
+  v = 1.0;
+  p = 1/gamma;
+  double nx =  0.5;
+  double ny = 0.5;
+  roeflux(nx, ny, rho, u, v, p, rho, u, v, p, gamma, f);
+  printf("%f\n", f[0]);
+  assert(abs(f[0]-1.0) < 1e-10);
+  printf("%f\n", f[1]);
+  assert(abs(f[1]-1.3571) < 1e-10);
+  printf("%f\n", f[2]);
+  assert(abs(f[2]-1.3571) < 1e-10);
+  printf("%f\n", f[3]);
+  assert(abs(f[3]-3.5) < 1e-10);
+  
+
+  return 0;
+
+}
