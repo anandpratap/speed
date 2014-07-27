b4c9f47f01436e207aced6c9cc87bf9837e98827
Anand Pratap Singh <anandps@f22.me>
2014-07-27 02:47:09 -0400

    Added openmp directives for calculation of roe flux, for 1000 iterations, grid size of 281x51, 1 proc-> 10.3 sec, 2 -> 7.831 sec, 3 -> 7.02 sec and 4 -> 7.231, using two is a good tradeoff.

--- src/residual.h
+++ src/residual.h
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <cilk/cilk.h>
+
 // Confession: the default abs() function returns only integer, used <cmath> and std::abs()
 //             wasted a day because of this stupid function
 
@@ -39,20 +41,31 @@ void residual(int ni, int nj, double Q[ni-1][nj-1][4],			\
   double flux_xi[ni][njm][4];
  
   grad_xi(ni, nj, rho, u, v, p, rlft_xi, ulft_xi, vlft_xi, plft_xi, rrht_xi, urht_xi, vrht_xi, prht_xi, order);
-  roeflux(nxi_x[:][:], nxi_y[:][:], rlft_xi[:][:], ulft_xi[:][:], vlft_xi[:][:], plft_xi[:][:],	\
-	  rrht_xi[:][:], urht_xi[:][:], vrht_xi[:][:], prht_xi[:][:], gamma, flux_xi[:][:]);
 
+#pragma omp parallel for
+  for(int i = 0; i < ni; i++) {
+    for(int j = 0; j < njm; j++) {
+      roeflux(nxi_x[i][j], nxi_y[i][j], rlft_xi[i][j], ulft_xi[i][j], vlft_xi[i][j], plft_xi[i][j], \
+	rrht_xi[i][j], urht_xi[i][j], vrht_xi[i][j], prht_xi[i][j], gamma, flux_xi[i][j]);
+    }
+  }
+  
   res[0:nim][0:njm][0:4] = res[0:nim][0:njm][0:4] - (flux_xi[1:nim][0:njm][0:4] - flux_xi[0:nim][0:njm][0:4]);
-
+  
   double rlft_eta[nim][nj], ulft_eta[nim][nj], vlft_eta[nim][nj], plft_eta[nim][nj];
   double rrht_eta[nim][nj], urht_eta[nim][nj], vrht_eta[nim][nj], prht_eta[nim][nj];
   double flux_eta[nim][nj][4];
-
-  grad_eta(ni, nj, rho, u, v, p, rlft_eta, ulft_eta, vlft_eta, plft_eta, rrht_eta, urht_eta, vrht_eta, prht_eta, order);
-  roeflux(neta_x[:][:], neta_y[:][:], rlft_eta[:][:], ulft_eta[:][:], vlft_eta[:][:], plft_eta[:][:], \
-  	  rrht_eta[:][:], urht_eta[:][:], vrht_eta[:][:], prht_eta[:][:], gamma, flux_eta[:][:]);
   
- 
+  grad_eta(ni, nj, rho, u, v, p, rlft_eta, ulft_eta, vlft_eta, plft_eta, rrht_eta, urht_eta, vrht_eta, prht_eta, order);
+  
+#pragma omp parallel for
+  for(int i = 0; i < nim; i++) {
+  for(int j = 0; j < nj; j++) {
+  roeflux(neta_x[i][j], neta_y[i][j], rlft_eta[i][j], ulft_eta[i][j], vlft_eta[i][j], plft_eta[i][j], \
+    rrht_eta[i][j], urht_eta[i][j], vrht_eta[i][j], prht_eta[i][j], gamma, flux_eta[i][j]);
+}
+}
+
   res[0:nim][0:njm][0:4] = res[0:nim][0:njm][0:4] - (flux_eta[0:nim][1:njm][0:4] - flux_eta[0:nim][0:njm][0:4]);
 
   
